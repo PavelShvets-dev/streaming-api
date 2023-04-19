a97634366b6fdf529f69b9920f800f50635ba5ad
Pavel Shvets <pn.shvets@MBP-Pavel-Shvets.local>
2023-04-20 02:15:26 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/videoplayer', (req, res) => {
+    const range = req.headers.range
+    const videoPath = './MU_10_04.mp4';
+    const videoSize = fs.statSync(videoPath).size
+    console.log('videoSize', videoSize);
+    const chunkSize = 1 * 1e6;
+    const start = Number(range.replace(/\D/g, ""))
+    const end = Math.min(start + chunkSize, videoSize - 1)
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers)
+    const stream = fs.createReadStream(videoPath, {
+        start,
+        end
+    })
+    stream.pipe(res)
+});
+
+app.get("/video", function (req, res) {
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+    const videoPath = req.query.id;
+    const videoSize = fs.statSync(`/opt/media/${videoPath}`).size;
+    console.log('videoSize', videoSize);
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+    const videoStream = fs.createReadStream(`/opt/media/${videoPath}`, { start, end });
+    videoStream.pipe(res);
+});
+
+app.listen(3000);
