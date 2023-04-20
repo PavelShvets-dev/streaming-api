c386fa785ecb2b5e9c1b448ad5097dc699c47aba
Pavel Shvets <pn.shvets@MBP-Pavel-Shvets.local>
2023-04-20 15:03:58 +0300

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 const app = express();
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
